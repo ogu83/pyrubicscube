88d467ca624128bc5def197c7f4f1ec1480678b0
ogu83 <oguzkoroglu@gmail.com>
2020-01-14 18:25:04 +0300

    Tutorial 4

--- opengl_tutorial.py
+++ opengl_tutorial.py
@@ -29,37 +29,88 @@ edges = (
     (5,4),
     (5,7),
     )
+    
+    
+surfaces = (
+    (0,1,2,3),
+    (3,2,7,6),
+    (6,7,5,4),
+    (4,5,1,0),
+    (1,5,7,2),
+    (4,0,3,6),
+)
+
+colors = (
+    (1,0,0),
+    (0,1,0),
+    (0,0,1),
+    (0,1,0),
+    (1,1,1),
+    (0,1,1),
+    (1,0,0),
+    (0,1,0),
+    (0,1,1),
+    (0,0,0),
+    (1,1,1),
+    (0,1,1),
+)
 
 def Cube():
+    
+    glBegin(GL_QUADS)
+         
+    for surface in surfaces:         
+        s = 0
+        for vertex in surface:            
+            s+=1
+            glColor3fv(colors[s])
+            glVertex3fv(verticies[vertex])            
+    glEnd()
+    
     glBegin(GL_LINES)
+    
     for edge in edges:
         for vertex in edge:
             glVertex3fv(verticies[vertex])
-
+    
     glEnd()
+        
 
 def main():
     pygame.init()
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslatef(0.0, 0.0, -5)
-    glRotatef(0, 0, 0, 0)
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50)
+    glTranslatef(0, 0, -25)
+    glRotatef(25, 2, 1, 0)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
+                
+            if event.type == pygame.KEYDOWN:
+                if (event.key == pygame.K_LEFT):
+                    glTranslatef(-0.5,0,0)
+                if (event.key == pygame.K_RIGHT):
+                    glTranslatef(0.5,0,0)
+                if (event.key == pygame.K_UP):
+                    glTranslatef(0,0.5,0)
+                if (event.key == pygame.K_DOWN):
+                    glTranslatef(0,-0.5,0)
+                    
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 4:
+                    glTranslatef(0,0,-1.0)
+                if event.button == 5:
+                    glTranslatef(0,0,1.0)
 
-        glRotatef(1, 3, 1, 1)
+        # glRotatef(1, 5, 5, 5)
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
         pygame.time.wait(10)
         
-main()        
-        
-
-
+main()
