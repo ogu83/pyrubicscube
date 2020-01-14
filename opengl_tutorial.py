8e25145d8656518365679c3453dfa4679dc63262
ogu83 <oguzkoroglu@gmail.com>
2020-01-14 16:35:09 +0300

    Cube Opengl Tutorial 1

--- opengl_tutorial.py
+++ opengl_tutorial.py
@@ -43,4 +43,23 @@ def main():
     display = (800,600)
     pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]))
+    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    glTranslatef(0.0, 0.0, -5)
+    glRotatef(0, 0, 0, 0)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        glRotatef(1, 3, 1, 1)
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        Cube()
+        pygame.display.flip()
+        pygame.time.wait(10)
+        
+main()        
+        
+
+
