33c3ca4fdc90b433f38d2a0cc237d19a3243c8aa
ogu83 <oguzkoroglu@gmail.com>
2020-01-16 23:56:28 +0300

    l

--- OpenGL_Geometry.py
+++ OpenGL_Geometry.py
@@ -6,7 +6,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 
 class Grid3D:
-    def __init__(self, size=1, depth_x = 50, depth_y = 50, color = GRAY):
+    def __init__(self, size = 1, depth_x = 50, depth_y = 50, color = GRAY):
         self.size = size
         self.color = color    
         self.depth_x = depth_x
@@ -16,48 +16,47 @@ class Grid3D:
         glBegin(GL_LINES)                
         glColor3fv(self.color)
         size = self.size
-        tran_matrix = np.array([self.depth_x/2+size, 0, self.depth_y/2+size])
-        for x in range(0,self.depth_x,self.size):
-            for y in range(0,self.depth_y,self.size):
-                v0 = np.array([x,0,y])
-                v1 = np.array([x+self.size,0,y])
-                v2 = np.array([x,0,y+self.size])
+        tran_matrix = np.array([self.depth_x/2 + size, 0, self.depth_y/2 + size])
+        for x in range(0, self.depth_x, self.size):
+            for y in range(0, self.depth_y, self.size):
+                v0 = np.array([x, 0, y])
+                v1 = np.array([x+self.size, 0, y])
+                v2 = np.array([x, 0, y+self.size])
                 glVertex3fv(v0 - tran_matrix)
                 glVertex3fv(v1 - tran_matrix)                
                 glVertex3fv(v0 - tran_matrix)
                 glVertex3fv(v2 - tran_matrix)
         glEnd()
         
-
 class Cube3D:
-    angle_x = [0,0]
-    angle_y = [0,0]
-    angle_z = [0,0]
+    angle_x = [0, 0]
+    angle_y = [0, 0]
+    angle_z = [0, 0]
 
     verticies=[
-    [1.0, -1.0, -1.0],
-    [1.0, 1.0, -1.0],
-    [-1.0, 1.0, -1.0],
-    [-1.0, -1.0, -1.0],
-    [1.0, -1.0, 1.0],
-    [1.0, 1.0, 1.0],
-    [-1.0, -1.0, 1.0],
-    [-1.0, 1.0, 1.0],
+        [1.0, -1.0, -1.0],
+        [1.0, 1.0, -1.0],
+        [-1.0, 1.0, -1.0],
+        [-1.0, -1.0, -1.0],
+        [1.0, -1.0, 1.0],
+        [1.0, 1.0, 1.0],
+        [-1.0, -1.0, 1.0],
+        [-1.0, 1.0, 1.0],
     ]
     
     edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7),
+        (0,1),
+        (0,3),
+        (0,4),
+        (2,1),
+        (2,3),
+        (2,7),
+        (6,3),
+        (6,4),
+        (6,7),
+        (5,1),
+        (5,4),
+        (5,7),
     )
        
     surfaces = (
@@ -67,13 +66,11 @@ class Cube3D:
         (4,5,1,0),
         (1,5,7,2),
         (4,0,3,6),
-    )
-    
-    
+    )       
 
     def __init__(self, size = 1, color_u=YELLOW, color_d=WHITE, color_f=BLUE, color_b=GREEN, color_l=ORANGE, color_r=RED):
         self.size = float(size)        
-        self.verticies = np.array(self.verticies) * size        
+        self.verticies = np.array(self.verticies) * size
         self.color_d = color_d #WHITE
         self.color_u = color_u #YELLOW
         self.color_b = color_b #GREEN                
@@ -83,18 +80,18 @@ class Cube3D:
         
     def getColorArray(self):
         return [            
-            self.color_b,#GREEN
-            self.color_l,#ORANGE             
-            self.color_f,#BLUE 
-            self.color_r,#RED                                              
-            self.color_u,#YELLOW
-            self.color_d,#WHITE            
+            self.color_b, #GREEN
+            self.color_l, #ORANGE             
+            self.color_f, #BLUE 
+            self.color_r, #RED                                              
+            self.color_u, #YELLOW
+            self.color_d, #WHITE            
         ]
         
     def draw(self):
         self.do_animations()
         
-        glBegin(GL_QUADS)        
+        glBegin(GL_QUADS)
         color_index = 0
         for surface in self.surfaces:
             glColor3fv(self.getColorArray()[color_index])
@@ -103,13 +100,12 @@ class Cube3D:
                 glVertex3fv(self.verticies[vertex])
         glEnd()
     
-        glBegin(GL_LINES)                
+        glBegin(GL_LINES)
         for edge in self.edges:
             for vertex in edge:
                 glColor3fv(GRAY)
                 glVertex3fv(self.verticies[vertex])        
         glEnd()
-
 
     def assing_vertex(self, vertex, v):
         for i in range(len(vertex)):
@@ -127,7 +123,7 @@ class Cube3D:
         rotation_matrix = np.array([[1.0, 0.0, 0.0],
                                     [0.0, cosa, -sina],
                                     [0.0, sina, cosa]])
-        self.assing_rotate_all_vertex(rotation_matrix)        
+        self.assing_rotate_all_vertex(rotation_matrix)
         
     def rotateY(self, angle):
         rad = angle * math.pi / 180
@@ -141,13 +137,21 @@ class Cube3D:
     def rotateZ(self, angle):
         rad = angle * math.pi / 180
         cosa = (math.cos(rad))
-        sina = (math.sin(rad))        
+        sina = (math.sin(rad))
         rotation_matrix = np.array([[cosa, -sina, 0.0],
                                     [sina, cosa, 0.0],
                                     [0.0, 0.0, 1.0]])
         self.assing_rotate_all_vertex(rotation_matrix)
-        
-    def do_animations(self, speed=5):
+
+    def translate_size(self, translate_matrix):
+        self.translate(np.array(translate_matrix) * self.size)
+
+    def translate(self, translate_matrix):
+        for vertex in self.verticies:
+            v = np.array(vertex) + np.array(translate_matrix)
+            self.assing_vertex(vertex, v)
+      
+    def do_animations(self, speed=15):
         if (self.angle_x[0] < self.angle_x[1]):
             self.angle_x[0] += speed
             self.rotateX(speed)
@@ -174,12 +178,12 @@ class Cube3D:
         
     def animated_rotateX(self,angle):
         if (not self.is_on_animation()):
-            self.angle_x[1]+=angle
+            self.angle_x[1] += angle
         
     def animated_rotateY(self,angle):
         if (not self.is_on_animation()):
-            self.angle_y[1]+=angle
+            self.angle_y[1] += angle
         
-    def animated_rotateZ(self,angle):        
+    def animated_rotateZ(self,angle):
         if (not self.is_on_animation()):
-            self.angle_z[1]+=angle
+            self.angle_z[1] += angle
--- Simulation3.py
+++ Simulation3.py
@@ -14,7 +14,7 @@ class Simulation3:
     left_click_on = False
     right_click_on = False
     
-    last_mouse_pos = (0,0)
+    last_mouse_pos = (0, 0)
     mouse_sensitivity = 50
     
     on_key_w = False
@@ -33,12 +33,13 @@ class Simulation3:
         pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
         glEnable(GL_DEPTH_TEST)
 
-        gluPerspective(60, (self.win_width/self.win_height), 0.1, 100)
+        gluPerspective(60, (self.win_width / self.win_height), 0.1, 100)
         glRotatef(45, 0.5, -0.5, -0.125)
-        glTranslatef(-25,-25,-25)
+        glTranslatef(-25, -25, -25)
         
         cube = Cube3D(4)
-        grid = Grid3D(4, 64, 64)
+        grid = Grid3D(4, 32,32)
+#        cube.translate_size([0, 1, 0]) 
         
         while True:       
             for event in pygame.event.get():
@@ -73,13 +74,13 @@ class Simulation3:
                         self.on_key_d = False
                 elif event.type == pygame.MOUSEMOTION:
                     if (self.left_click_on):
-                        cam_x = (event.pos[0] - self.last_mouse_pos[0])/self.mouse_sensitivity
-                        cam_y = (event.pos[1] - self.last_mouse_pos[1])/self.mouse_sensitivity
+                        cam_x = (event.pos[0] - self.last_mouse_pos[0]) / self.mouse_sensitivity
+                        cam_y = (event.pos[1] - self.last_mouse_pos[1]) / self.mouse_sensitivity
                         glTranslatef(cam_x, 0, cam_y)
                         self.last_mouse_pos = event.pos
                     elif (self.right_click_on):
-                        cam_y = (event.pos[0] - self.last_mouse_pos[0])/self.mouse_sensitivity
-                        cam_z = (event.pos[1] - self.last_mouse_pos[1])/self.mouse_sensitivity
+                        cam_y = (event.pos[0] - self.last_mouse_pos[0]) / self.mouse_sensitivity
+                        cam_z = (event.pos[1] - self.last_mouse_pos[1]) / self.mouse_sensitivity
                         glTranslatef(0, cam_y, cam_z)
                         self.last_mouse_pos = event.pos
                 elif event.type == pygame.MOUSEBUTTONDOWN and event.button == LEFT:
@@ -96,13 +97,13 @@ class Simulation3:
                     self.last_mouse_pos = (0,0)
                     
             if (self.on_key_w):
-                glTranslatef(1/self.mouse_sensitivity,0,0)
+                glTranslatef(1 / self.mouse_sensitivity, 0, 0)
             if (self.on_key_s):
-                glTranslatef(-1/self.mouse_sensitivity,0,0)
+                glTranslatef(-1 / self.mouse_sensitivity, 0, 0)
             if (self.on_key_d):
-                glTranslatef(0,-1/self.mouse_sensitivity,0)
+                glTranslatef(0, -1 / self.mouse_sensitivity, 0)
             if (self.on_key_a):
-                glTranslatef(0,1/self.mouse_sensitivity,0)
+                glTranslatef(0, 1 / self.mouse_sensitivity, 0)
                                             
             glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)                      
                         
@@ -110,4 +111,4 @@ class Simulation3:
             cube.draw()
             
             pygame.display.flip()
-            pygame.time.wait(1)
+            pygame.time.wait(20)
